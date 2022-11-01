bfb6a7a2cc706becb633316b884358bfe61b7430
Drishti Jain <drishtijain2103@gmail.com>
2022-11-01 19:06:17 +0530

    added files

--- final/js/offcampus.js
+++ final/js/offcampus.js
@@ -1,37 +1,13 @@
-const form = document.querySelector("form"),
-        nextBtn = form.querySelector(".nextBtn"),
-        backBtn = form.querySelector(".backBtn"),
-        allInput = form.querySelectorAll(".first input");
 
 
-nextBtn.addEventListener("click", ()=> {
-    allInput.forEach(input => {
-        if(input.value != ""){
-            form.classList.add('secActive');
-        }else{
-            form.classList.remove('secActive');
-        }
-    })
-})
+document.getElementById("clickme").addEventListener("click",()=>{    
+	const output=document.querySelector(".output");
+	 output.textContent="Congratulations you are chosen for the role of Software Quality Assurance (QA) / Testing. /n The prob is of the role  14.000000000000002 %";
+	document.querySelector(".output").style.color= "white";
+	});
 
-backBtn.addEventListener("click", () => form.classList.remove('secActive'));
-
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-        elem.innerHTML = width + "%";
-      }
-    }
-  }
-}
+  document.getElementById("click").addEventListener("click",()=>{    
+    const output=document.querySelector(".output");
+     output.textContent="Based upon the previous years data you may not get placed. But keep working hard!";
+    document.querySelector(".output").style.color= "white";
+    });
