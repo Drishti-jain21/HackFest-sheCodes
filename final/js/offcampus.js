6bc700e1965350c1dbe0863391d4e641bdb2c2c9
Drishti Jain <drishtijain2103@gmail.com>
2022-11-01 09:41:04 +0530

    changed UI

--- final/js/offcampus.js
+++ final/js/offcampus.js
@@ -15,3 +15,23 @@ nextBtn.addEventListener("click", ()=> {
 })
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
