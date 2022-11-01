647292a342958f52b49e4ec380f29d2f245be6d2
Drishti Jain <drishtijain2103@gmail.com>
2022-11-01 19:40:50 +0530

    changed files

--- final/js/offcampus.js
+++ final/js/offcampus.js
@@ -1,13 +1,1 @@
 
-
-document.getElementById("clickme").addEventListener("click",()=>{    
-	const output=document.querySelector(".output");
-	 output.textContent="Congratulations you are chosen for the role of Software Quality Assurance (QA) / Testing. /n The prob is of the role  14.000000000000002 %";
-	document.querySelector(".output").style.color= "white";
-	});
-
-  document.getElementById("click").addEventListener("click",()=>{    
-    const output=document.querySelector(".output");
-     output.textContent="Based upon the previous years data you may not get placed. But keep working hard!";
-    document.querySelector(".output").style.color= "white";
-    });
